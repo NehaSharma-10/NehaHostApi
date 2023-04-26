b8c5accf6e50f54104c8bd428ae59eff8597255b
NehaSharma-10 <shrmatwinkle246@gmail.com>
2023-04-26 12:02:48 +0530

    host api commit msg

--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,5 @@ const router = express.Router();
 const { getAllProducts } = require("../controllers/products");
 
 router.route("/").get(getAllProducts);
-// router.route("/testing").get(getAllTestingProducts);
 
 module.exports = router;
